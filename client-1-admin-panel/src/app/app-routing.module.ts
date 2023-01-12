8d693f0200830eccd9550522453539367553721b
shree42N <riti91557@gmail.com>
2023-01-12 18:12:34 +0530

    changes

--- client-1-admin-panel/src/app/app-routing.module.ts
+++ client-1-admin-panel/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "auth",
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
